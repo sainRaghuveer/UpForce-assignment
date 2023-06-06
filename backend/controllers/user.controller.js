87beef9b779bf1fb440fdb75738b3b29106ff184
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-06 13:04:48 +0530

    user post

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,9 +2,19 @@ const { userModel } = require("../models/user.model")
 
 
 exports.postUser = async (req, res) => {
+    const {email} = req.body;
     try {
         const userAlreadyExist = await userModel.findOne({email});
+        if(userAlreadyExist.length >0 ){
+           return res.status(409).send({ "msg": "user already exists" });
+        }else{
+            const user = new userModel(req.body);
+            await user.save();
+            res.status(200).send({ "msg": "User saved successful", "status": true });
+        }
     } catch (error) {
-
+        console.log(error.message);
+        res.status(500).send({ "msg": "Something went wrong", "error": error.message });
     }
-}
+};
+
