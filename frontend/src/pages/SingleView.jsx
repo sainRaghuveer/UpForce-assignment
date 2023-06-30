74ba231b90080f0b5dbfad6aaa6d5391e10c0d6a
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-30 20:28:07 +0530

    unwanted components removed

--- frontend/src/pages/SingleView.jsx
+++ frontend/src/pages/SingleView.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import SingleUSerCard from '../components/SingleUserCard';
-import { Skeleton, Spinner } from '@chakra-ui/react';
+import { Spinner } from '@chakra-ui/react';
 
 const SingleView = () => {
   const { id } = useParams();
